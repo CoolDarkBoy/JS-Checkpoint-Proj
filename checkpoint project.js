7579334ba8c89499f502d1c28654576b493b61ed
Tanmay Aloorkar <40850550+CoolDarkBoy@users.noreply.github.com>
2024-07-26 01:42:08 +0530

    Update checkpoint project.js

--- checkpoint project.js
+++ checkpoint project.js
@@ -3,8 +3,10 @@ let total = 3000;
 console.log("Leap Years between 2000 and 3000 are as follows : \n")
 
 for (let i=2000; i<=total; i++) {
-    if (i%4===0 && i%400===0) {
-        if (i%100!==0) {
+    if (i%4===0) {
+        if (i%100!==0 && i%400===0) {
+            console.log(i)
+        } else {
             console.log(i)
         }
     }
